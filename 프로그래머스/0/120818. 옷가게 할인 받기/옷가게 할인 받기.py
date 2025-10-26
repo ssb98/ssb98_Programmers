71c63127bf525ee2fcfd34d10e303dfddc130f75
ssb98 <ssb980908@gmail.com>
2025-10-26 23:01:28 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,10 @@
+def solution(price):
+    if price >= 100000 and price < 300000:
+        price = price * 0.95
+    elif price >= 300000 and price < 500000:
+        price = price * 0.9
+    elif price >= 500000:
+        price = price * 0.8
+    elif price < 100000:
+        price
+    return int(price)
