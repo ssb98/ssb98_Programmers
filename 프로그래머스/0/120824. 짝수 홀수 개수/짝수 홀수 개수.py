ac46530bcd9ee73dd693a7679f13d4dfd21c2fac
ssb98 <ssb980908@gmail.com>
2025-10-26 22:02:59 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,12 @@
+def solution(num_list):
+    answer = []
+    a = 0
+    b = 0
+    for num in num_list:
+        if num%2 == 0:
+            a+=1
+        else:
+            b+=1
+    answer.append(a)
+    answer.append(b)
+    return answer
