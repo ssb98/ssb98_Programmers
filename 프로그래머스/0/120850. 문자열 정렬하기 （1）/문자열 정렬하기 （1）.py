63c6cf7da007b1a9634fd7cbf5cb3acd502b0fad
ssb98 <ssb980908@gmail.com>
2025-10-27 14:16:13 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.03 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -4,3 +4,11 @@ def solution(my_string):
         if idx in '0123456789' :
             answer.append(int(idx))
     return sorted(answer)
+
+# def solution(my_string):
+#     answer = []
+#     for i in my_string:
+#         if i.isdigit():
+#             answer.append(int(i))
+#     answer.sort()
+#     return answer
