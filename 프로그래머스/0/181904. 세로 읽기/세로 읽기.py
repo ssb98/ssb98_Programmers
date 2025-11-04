5fdd6e66c1cb930a8c40cad31a139d307217d620
ssb98 <ssb980908@gmail.com>
2025-11-04 11:04:33 +0900

    [level 0] Title: 세로 읽기, Time: 0.37 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
@@ -2,16 +2,14 @@ def solution(my_string, m, c):
     answer = ''
     my = ''
     string=[]
-    
     for i in my_string:
         my += i
         if len(my) == m:
             string.append(my)
-            my = ''
-            
+            my = ''  
     for j in string:
-        answer += j[c-1]
-        
+        answer += j[c-1] 
     return answer
 
-## 스트링을 m개씩 나눠서 남고, 인덱싱으로 c위치를 answer에 담기
+# def solution(s, m, c):
+#     return s[c-1::m]
