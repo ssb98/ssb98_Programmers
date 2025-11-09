3ab3338b2c79dc822b0116bdd617d83a92b4f307
ssb98 <ssb980908@gmail.com>
2025-11-09 20:18:44 +0900

    [level 0] Title: A로 B 만들기, Time: 0.23 ms, Memory: 9.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
@@ -0,0 +1,10 @@
+def solution(before, after):
+    answer = 0
+    before=sorted(before)
+    after=sorted(after)
+    
+    if before==after:
+        answer = 1
+    else:
+        answer = 0
+    return answer
