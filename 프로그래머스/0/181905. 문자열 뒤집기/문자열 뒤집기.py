cebb93d0a4c2b5a740345e64a6cd50d04eb5f2c6
ssb98 <ssb980908@gmail.com>
2025-11-09 17:47:28 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.00 ms, Memory: 9.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
@@ -0,0 +1,7 @@
+def solution(my_string, s, e):
+    answer = ''
+    if s != 0:
+        answer = my_string[:s] + my_string[e:s-1:-1] + my_string[e+1:]
+    else:
+        answer = my_string[e::-1] + my_string[e+1:]
+    return answer
