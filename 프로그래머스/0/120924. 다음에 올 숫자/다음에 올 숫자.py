e1d719ce1648b0d31173d5546ff37f94938307e1
ssb98 <ssb980908@gmail.com>
2025-11-09 21:41:49 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -0,0 +1,8 @@
+def solution(common):
+    answer = 0
+    a,b,c = common[:3]
+    if (b-a) == (c-b):
+        return common[-1]+(b-a)
+    else:
+        return common[-1] * (b//a)
+    return answer
